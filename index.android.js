e93f34652d667f21819892ec43b57bff1e3a414f
Riza Fahmi <rizafahmi@gmail.com>
2017-06-29 11:02:28 +0700

    Add secret.js

--- index.android.js
+++ index.android.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { AppRegistry, StyleSheet, Button, View } from 'react-native'
 import Video from 'react-native-video'
 
+import CLIENT_ID from './secret'
+
 export default class RNAppsCoast extends Component {
   constructor (props) {
     super(props)
@@ -28,7 +30,7 @@ export default class RNAppsCoast extends Component {
         <Button onPress={() => this.togglePlay()} title={title} />
         <Video
           source={{
-            uri: 'https://api.soundcloud.com/tracks/316568736/stream?client_id=0f2c62e4540699fd850f4beb2f9583d6'
+            uri: `https://api.soundcloud.com/tracks/316568736/stream?client_id=${CLIENT_ID}`
           }}
           ref='audio'
           volume={1.0}
